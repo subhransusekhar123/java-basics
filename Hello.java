d3658c14a49e47b417381b95fc2e7a573fa6c1a1
unknown <subhransusekharsahu29@gmail.com>
2024-04-23 07:05:11 +0530

    basics

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Hello
+ */
+class Hello {
+    public static void main(String a[]) 
+    {
+        int x;
+        int y;
+        x = 5;
+        y = 6;
+        System.out.println(x + y);
+    }
+}
+
+// javacode ==> compiler(javac) ==> bytecode.class ==> jre(java runtime environment) >> jvm(java virtual machine)
